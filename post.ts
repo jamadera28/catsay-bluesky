b12d7a29389314e1030140b770eb725ae31cc7e0
Joseph Madera <jamadera28@gmail.com>
2024-11-24 12:26:18 -0500

    remove cron usage

--- post.ts
+++ post.ts
@@ -1,6 +1,5 @@
 import { AtpAgent } from "@atproto/api";
 import * as dotenv from "dotenv";
-import { CronJob } from "cron";
 import * as process from "process";
 
 dotenv.config();
@@ -40,11 +39,3 @@ async function main() {
 
 main();
 
-// Run this on a cron job
-
-//const scheduleExpressionMinute = "* * * * *"; // Run once every minute for testing
-//const scheduleExpression = "0 */3 * * *"; // Run once every three hours in prod
-
-//const job = new CronJob(scheduleExpression, main); // change to scheduleExpressionMinute for testing
-
-//job.start();
