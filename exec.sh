cbddd37e859e2ac0ad0a4b7c70334e03f3c6f37a
Joseph Madera <jamadera28@gmail.com>
2024-12-14 21:21:53 -0500

    fix issue where empty tweet body could get posted

--- exec.sh
+++
@@ -1,8 +0,0 @@
-#! /bin/sh
-
-
-until $(fortune -n 50 | java Catsay | node post.js); do
-    echo "Retrying..."
-done
-
-echo "Done."
--- post.ts
+++ post.ts
@@ -4,27 +4,22 @@ import * as process from "process";
 
 dotenv.config();
 
-// Create a Bluesky Agent
 const agent = new AtpAgent({
   service: "https://bsky.social",
 });
 
 let body: string = "";
 
-// Listen for data events on stdin to collect chunks of input
 process.stdin.on("data", (chunk: Buffer) => {
-  body += chunk.toString(); // Convert Buffer to string and append
+  body += chunk.toString(); 
 });
 
-// Listen for the end event to process the collected input
 process.stdin.on("end", () => {
-  //console.log("Collected Input:", body); // Or process the input as needed
+  //console.log("Collected Input:", body); 
 });
 
 async function main() {
-  //console.log(process.env.BLUESKY_HANDLE!);
-  //console.log(process.env.BLUESKY_PASSWORD!);
-  if (body.length > 300) {
+  if (body.length > 300 || body.length < 10) {
     return 1;
   }
   await agent.login({
